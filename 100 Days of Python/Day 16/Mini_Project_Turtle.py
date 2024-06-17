33eb9d93cee0ab6e8ea6939f0d0cf3a224a077a6
methezain <methealizain@gmail.com>
2024-06-17 18:46:40 +0500

    turtle library

---
+++ 100 Days of Python/Day 16/Mini_Project_Turtle.py
@@ -0,0 +1,11 @@
+from turtle import Turtle , Screen
+
+timmy = Turtle()  
+
+for colour in ["purple", "orange", "red", "black", "cyan", "navy"]:
+    timmy.color(colour)
+    timmy.circle(75)
+    timmy.left(60)
+
+myScreen = Screen()
+myScreen.exitonclick()
--- 100 Days of Python/Day 16/turtle_module.py
+++ 100 Days of Python/Day 16/turtle_module.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
 
 myTurtle = Turtle()
-myTurtle.color("navy", "green")
-myTurtle.forward(100)
+myTurtle.color("red")
 myTurtle.shape("turtle")
 
+myTurtle.backward(100)
+
+for i in range(0,4):
+    myTurtle.forward(200)
+    myTurtle.left(90)
+    
 myCanvas = Screen()
 myCanvas.exitonclick() 
