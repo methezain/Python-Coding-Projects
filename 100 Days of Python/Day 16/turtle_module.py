23d245679659d30a0865347139960e1f427d417e
methezain <methealizain@gmail.com>
2024-06-17 16:49:57 +0500

    updates just

---
+++ 100 Days of Python/Day 16/turtle_module.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+myTurtle = Turtle()
+myTurtle.color("navy", "green")
+myTurtle.forward(100)
+myTurtle.shape("turtle")
+
+myCanvas = Screen()
+myCanvas.exitonclick() 
